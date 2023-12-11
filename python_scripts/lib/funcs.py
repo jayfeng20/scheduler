20f2ec12133a67bc7d268f61c5c1293845ad9909
joannazhang7 <zhang.joanna81@gmail.com>
2023-12-11 18:30:28 -0500

    start_of_week done

--- python_scripts/lib/funcs.py
+++ python_scripts/lib/funcs.py
@@ -1,16 +1,17 @@
 import datetime
 
 
-# returns a dictionary with keys day, month, year only
+# returns a dictionary with keys day, month, year only; YYYY-MM-DD
 def start_of_week():
-    #datetime format: YYY-MM-DD HH:MM:SS.ssssss
+    #datetime format: YYYY-MM-DD HH:MM:SS.ssssss
     current_datetime = datetime.datetime.now()
     # .weekday() gets day of the week as int; Mon = 0 Sun = 6
     start = current_datetime - datetime.timedelta(days=current_datetime.weekday())
-    #.date() extracts the date part (w/o time info)
+    #.date() extracts the date part(w/o time info)
     return start.date()
 
 
-#tests
+#test
 result = start_of_week()
-print("Start of this week" + result)
+print(result) #gives 2023-12-11
+
