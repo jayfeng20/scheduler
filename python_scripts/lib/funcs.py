ed61a84db89785e880f1328a0c11fb6fbf3b3f9e
joannazhang7 <zhang.joanna81@gmail.com>
2023-12-11 18:42:46 -0500

    edited start_of_week to return dict

--- python_scripts/lib/funcs.py
+++ python_scripts/lib/funcs.py
@@ -7,8 +7,9 @@ def start_of_week():
     current_datetime = datetime.datetime.now()
     # .weekday() gets day of the week as int; Mon = 0 Sun = 6
     start = current_datetime - datetime.timedelta(days=current_datetime.weekday())
-    #.date() extracts the date part(w/o time info)
-    return start.date()
+    date = start.date() #date format: YYYY-MM-DD
+
+    return {'day': date.day, 'month': date.month, 'year': date.year}
 
 
 #test
