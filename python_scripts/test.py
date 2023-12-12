6abbd3ab6b57a63a0971ab916ed9a761d3c010d0
jayfeng20 <jonathanfeng2001@gmail.com>
2023-12-11 21:03:30 -0500

    test conpleted

--- python_scripts/test.py
+++ python_scripts/test.py
@@ -15,7 +15,6 @@ assert expected_time <= 24 and 1 <= expected_time
 due_in = 1
 assert due_in >= 1 and due_in <= 7 # due in at least 1 day and at most 1 week
 
-
 # mysql config
 host = os.environ.get("DB_ENDPOINT")
 user = os.environ.get("DB_USERNAME")
@@ -35,48 +34,7 @@ client = OpenAI(
   api_key=key,
 )
 
-
-
-alr_booked = []
-
-system_message = """
-You are a concise and precise assistant. You find the most reasonable time slot(s) to assign this task within the next week
-and before the due date.
-Your output is in the form of a json object with 2 fields named "start_time" and "end_time" whose contents are 
-python datetime objects. They represent the time slot(s) you chose to schedule the task.
-"""
-
-user_message = "Can you please find the best time slot(s) for me to do the following task?" + \
-f"""
-time right now: {current_datetime}
-expected time: {expected_time} hours to finish, 
-due in: {due_in} days.
-Already booked slots are: {', '.join(alr_booked)}.
-"""
-
-
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo-1106",
-  response_format={ "type": "json_object" },
-  
-  
-  messages=[
-    {"role": "system", "content": system_message},
-    {"role": "user", "content": user_message}
-  ],
-  top_p=0.3 # TODO: need finetuning
-
-)
-
-# ai's response
-response = completion.choices[0].message.content
-print(response)
-
-# the new time slots to be added
-start_time, end_time = response['start_time'], response['end_time']
-
-
-
+# start the connectionn
 try:
     connection = mysql.connector.connect(**db_config)
     if connection.is_connected():
@@ -84,15 +42,58 @@ try:
         
     # Create a cursor to execute SQL queries
     cursor = connection.cursor()
-
     use_db_query = f"USE {db}"
     cursor.execute(use_db_query)
     print(f"Using database {db}")
 
   # store data
     f.create(current_datetime, cursor)
-    f.insert(start_time=start_time, end_time=end_time, cursor=cursor)
+    print('new table created')
 
+    alr_booked = []
+    rows1, rows2 = f.retrieve(current=current_datetime, cursor=cursor)
+    start_times = [rows1[0] for row in rows1].extend([rows2[0] for row in rows2])
+    end_times = [rows1[1] for row in rows1].extend([rows2[1] for row in rows2])
+    for i in range(len(start_times)):
+        alr_booked.append(f"{start_times[i]} to {end_times[i]}")
+
+    print(f'alr_booked: {alr_booked}')
+
+    system_message = """
+    You are a concise and precise assistant. You find the most reasonable time slot(s) to assign this task within the next week
+    and before the due date.
+    Your output is in the form of a json object with 2 fields named "start_time" and "end_time" whose contents are 
+    python datetime objects. They represent the time slot(s) you chose to schedule the task.
+    """
+
+    user_message = "Can you please find the best time slot(s) for me to do the following task?" + \
+    f"""
+    time right now: {current_datetime}
+    expected time: {expected_time} hours to finish, 
+    due in: {due_in} days.
+    Already booked slots are: {', '.join(alr_booked)}.
+    """
+
+
+    completion = client.chat.completions.create(
+      model="gpt-3.5-turbo-1106",
+      response_format={ "type": "json_object" },
+      messages=[
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": user_message}
+      ],
+      top_p=0.3 # TODO: need finetuning
+    )
+
+    # ai's response
+    response = completion.choices[0].message.content
+    print(response)
+
+    # the new time slots to be added
+    start_time, end_time = response['start_time'], response['end_time']
+
+    f.insert(start_time=start_time, end_time=end_time, cursor=cursor)
+    print('new time slot created and added')
 
 except mysql.connector.Error as e:
     print(f"Error connecting to AWS RDS: {e}")
